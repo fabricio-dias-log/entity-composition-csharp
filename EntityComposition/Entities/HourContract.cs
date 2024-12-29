8f342a3840f091bb9f4d61619ff4de0b709ea947
Fabricio Dias <fabricio270909@gmail.com>
2024-12-28 23:17:34 -0300

    Entities structured

---
+++ EntityComposition/Entities/HourContract.cs
@@ -0,0 +1,25 @@
+﻿namespace EntityComposition.Entities;
+
+public class HourContract
+{
+    public DateTime Date { get; set; }
+    public double ValuePerHour { get; set; }
+    public int Hours { get; set; }
+
+    public HourContract()
+    {
+        
+    }
+
+    public HourContract(DateTime date, double valuePerHour, int hours)
+    {
+        this.Date = date;
+        this.ValuePerHour = valuePerHour;
+        this.Hours = hours;
+    }
+
+    public double TotalValue()
+    {
+        return this.Hours * this.ValuePerHour;  
+    }
+}
